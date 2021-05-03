59c7196b965c1511bd1729c1e188bf307139a3aa
Adrian Sims <adriansims62781@gmail.com>
2021-05-03 12:58:00 +0200

    Add project files.

---
+++ Cast2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cast2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var o = (i % 2 == 1) ? (object)$"{Guid.NewGuid()}" : (object)(int)2;
+                var s = GetString(o);
+            }
+        }
+
+        private static string GetString(object o)
+        {
+            return (string)o;
+        }
+        private static string GetString2(object o)
+        {
+            return o.ToString();
+        }
+    }
+}
